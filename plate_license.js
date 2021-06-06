15dcc43e985a31590407f7dac36a4ab8cdb4a60b
Zidian Lyu <zidianlyu@gmail.com>
2021-06-06 13:48:34 -0700

    Build a class with class variable

--- plate_license.js
+++ plate_license.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+class PlateLicense {
+  // 构建class的时候第一个会跑的method
+  constructor() {
+    // 可以放class variable
+    this.licenses = [];
+  }
+}
+
+// 创建一个class instance
+const pl = new PlateLicense();
+// 使用class里面的variable
+console.log(pl.licenses);
