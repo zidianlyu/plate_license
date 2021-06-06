e0c18abed11422704a18fb13a787eb30d321a1fa
Zidian Lyu <zidianlyu@gmail.com>
2021-06-06 14:00:03 -0700

    #2 Add getAllLicenses method to return all licenses

--- plate_license.js
+++ plate_license.js
@@ -1,12 +1,22 @@
+const MOCK_LICENSE_OBJECT = {
+  license: '6LZD666', // string
+  registered: 1622953087393, // UTC timestamp
+  status: 'REGISTERED', // string
+};
+
 class PlateLicense {
   // 构建class的时候第一个会跑的method
   constructor() {
     // 可以放class variable
-    this.licenses = [];
+    this.licenses = [MOCK_LICENSE_OBJECT];
+  }
+
+  getAllLicenses() {
+    return this.licenses;
   }
 }
 
 // 创建一个class instance
 const pl = new PlateLicense();
-// 使用class里面的variable
-console.log(pl.licenses);
+
+console.log(pl.getAllLicenses());
