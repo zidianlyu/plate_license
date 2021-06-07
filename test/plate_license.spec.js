059523ab4ea892edc5ccf2eb55c58699f556bf45
Zidian Lyu <zidianlyu@gmail.com>
2021-06-06 19:18:03 -0700

    Add testing for getAllLicenses

--- test/plate_license.spec.js
+++ test/plate_license.spec.js
@@ -1,8 +1,19 @@
 const PlateLicense = require('../plate_license');
+let pl = new PlateLicense();
+
+// This function will be called before running every test.
+beforeEach(() => {
+  pl = new PlateLicense();
+});
 
 describe('#1 Generate PlateLicense class', () => {
   it('successfully generate the instance', () => {
-    const pl = new PlateLicense();
     expect(pl).toBeDefined();
+  });
+});
+
+describe('#2 Track records', () => {
+  it('successfully get all plate licenses', () => {
+    expect(pl.getAllLicenses().length).toBe(1);
   });
 });
